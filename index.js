9270c003947d45f54efb165870c6b7c01c594f46
dinu0000 <102976558+dinu0000@users.noreply.github.com>
2022-11-20 12:57:21 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import Student from './studentmodel.js';
+
+mongoose.connect('mongodb+srv://dinu0000:RLQHO0usK6afBaNN@dinuks.zqu91qf.mongodb.net/?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=> console.log('DB connected'))
+.catch((error)=> console.log('DB has connection error'));
+const app = express();
+
+app.use((express.json()))
+app.use((express.urlencoded({extended: true})))
+
+app.get('/', (req,res)=>{
+  res.send('Server is live')
+})
+
+
+app.post('/createstudent', async(req,res)=>{
+
+    const {roll,department,contact,name,identity} = req.body;
+    console.log(req.body)
+  
+   const user = new Student({roll:roll, name: name, department: department,contact:contact,identity:identity})
+   try {
+    await user.save()
+    console.log('student added')
+    res.send('Success');
+   }catch(error){
+    console.log('Student not created')
+    res.send('error')
+   }
+})
+
+
+app.get('/studentdata', async(req,res)=>{
+    const user = await Student.find()
+
+    res.json(user);
+})
+app.listen(5000,()=>{
+    console.log('Running on port 5000')
+})
